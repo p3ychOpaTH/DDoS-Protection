e205ea62921e8c6812d9493f97deaa457c34d898
p3ychOpaTH <xranbon@gmail.com>
2026-05-09 11:53:21 +0300

    Delete DDoS-Protection-fixed.py
    Signed-off-by: p3ychOpaTH <xranbon@gmail.com>

--- DDoS-Protection-fixed.py
+++
@@ -1,6 +0,0 @@
-DELETED BY REQUEST
-
-The file DDoS-Protection-fixed.py was added automatically by Copilot earlier.
-Per your request it has been removed and replaced with this stub. If you
-prefer it fully deleted, please delete the file from the repository or
-confirm and I will remove the content and leave a note in the history.
